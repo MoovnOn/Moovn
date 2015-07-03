b7f4051a00212fdd88f691f350178b3264346940
Andy Baird <Andy.Baird@icloud.com>
2015-07-03 15:20:05 -0400

    added search href - broken

--- Moovn/static/js/views.js
+++ Moovn/static/js/views.js
@@ -1,4 +1,4 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"views":[function(require,module,exports){
-var views={"city-comp":"City Comparison","city":"City View","not-found":"<h1>404 Not found!</h1>","search":"<form class=\"pure-form\">\n  <input class = \"search\">\n</form>"};
+var views={"city-comp":"City Comparison","city":"City View","not-found":"<h1>404 Not found!</h1>","search":"<form class=\"pure-form\">\n  <input class = \"search\">\n  <a class = \"submit\" href=\"#city/<%-  %>\">Submit\n  </a>\n  <input class = \"search-comp\">\n  <button class = \"compare\">Compare\n  </button>\n</form>"};
 if (typeof module !== "undefined" && module.exports) { module.exports = views; }
 },{}]},{},[]);
