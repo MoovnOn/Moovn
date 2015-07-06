e5dde8cc6a81197bf28befe39529526014666e64
Andy Baird <Andy.Baird@icloud.com>
2015-07-05 20:29:12 -0400

    added basic autocomplete functionality to city and city comp searching, added basic styling to autocomplete Ul

--- src/js/controllers/search-controller.js
+++ src/js/controllers/search-controller.js
@@ -3,10 +3,10 @@ var _ = require('underscore');
 var views = require('views');
 var router = require('../router');
 var show = require('../show');
+var autocomplete = require('jquery-ui');
 
 router.route('', 'search', function (){
   show('search');
-
   $('.compare').on('click', function(e) {
   	e.preventDefault();
 
@@ -18,16 +18,43 @@ router.route('', 'search', function (){
   $('.compare-form').on('submit', function(e) {
   	e.preventDefault();
 
-  	var searchCity = $('.search-city').val()
+  	var searchCity = $('.search-city').val();
   	var compareCity = $('.search-city-comp').val();
 
   	if (compareCity != '' && searchCity != '') {
-  		router.navigate("search/" + searchCity + "/" + compareCity, {trigger: true})
+  		router.navigate("search/" + searchCity + "/" + compareCity, {trigger: true});
   	} else if (compareCity === '' && searchCity != '') {		
   		router.navigate("search/" + searchCity, {trigger: true});
   	} else {
-  		alert('Please enter the city you would like to see')
+  		alert('Please enter the city you would like to see');
   	}
 	});
+  
+    
+  $(function() {
+      var availableTags = [
+        "Austin, TX", 
+        "Durham, NC", 
+        "Charlotte, NC",
+        "Fort Collins, CO",
+        "Colorado Springs, CO",
+        "Nashville, TN",
+        "Seattle, WA"
+      ];
+      $("#tags").autocomplete({
+        source: availableTags,
+        messages: {
+          noResults: '',
+          results: function() {}
+      }
+      });
+       $("#tags2").autocomplete({
+        source: availableTags,
+        messages: {
+          noResults: '',
+          results: function() {}
+      }
+      });
+  });
 
 });
