53e15e560b8541f6212116189fa28ffbc4c1463a
Jeremy Gresham <greshjs@gmail.com>
2015-07-07 13:32:23 -0400

    changed models around, fixed city making script

--- Moovn/api/urls.py
+++ Moovn/api/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
+
+from . import views
+
 router = routers.DefaultRouter()
 
-urlpatterns = []
+
+urlpatterns = [
+    url(r'boundary/(?P<id>[\w]+)/', views),
+
+]
 
 urlpatterns.extend(router)
--- Moovn/api/views.py
+++ Moovn/api/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from rest_framework import ViewSets
+from django.shortcuts import render, redirect, get_object_or_404
+#from rest_framework import ViewSets
 
-from geo.models import 
+from geo.models import City, Boundary, Name
 
-class BoundaryViewSet(ViewSets.ModelViewSet):
-    model = Boundary
+@api_view(['GET',])
+def city_boundary_view(request):
+    city_name = request.GET.get('name')
+    city_state = request.GET.get('state')
+    name = get_object_or_404(Name, name=city_name, state=city_state)
+
+    response = HttpResponse(data=name.city.boundary.data)
+    response.status_code = 200
+
+    return response
