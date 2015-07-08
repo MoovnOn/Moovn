ad931a4064cbe48ad059256ed6ed46e396ef1640
Jeremy Gresham <greshjs@gmail.com>
2015-07-07 21:21:00 -0400

    fixed even more more more urls

--- Moovn/api/urls.py
+++ Moovn/api/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^boundary/(?P<state>[\w]+)/(?P<name>(?:[\w]+)\s?\-?(?:[\w]+))/$', views.city_boundary_view),
+    url(r'^boundary/(?P<state>[\w]+)/(?P<name>(?:[\w]+\s?\-?)+)/$', views.city_boundary_view),
     url(r'^homeprices/(?P<state>[\w\-]+)/(?P<city>[\w\-]+)/$', views.HomeView.as_view()),
     url(r'^celldata/(?P<state>[\w]+)/(?P<name>[\w]+)/$', views.cell_view),
 ]
