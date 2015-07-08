fc1f059bec6ef48e0ff8511d9e6db76d43d80f2a
Jeremy Gresham <greshjs@gmail.com>
2015-07-08 16:13:06 -0400

    merged

--- Moovn/api/views.py
+++ Moovn/api/views.py
@@ -5,13 +5,13 @@ from django.views.generic import View
 import requests
 import geojson
 from rest_framework import permissions
-from rest_framework.response import Response
+#from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 #from rest_framework import ViewSets
 
 from geo.models import City, Boundary, Name
 import xmltodict
-
+import json
 
 # @api_view(['GET',])
 # @permission_classes((permissions.AllowAny,))
@@ -47,7 +47,8 @@ def cell_view(request, state, name):
                 + "&json_format=" + "2" # 2 is suggested \
                 + "&apikey=" + apis('opensignal'))
 
-    return Response(signal)
+    signal = json.loads(signal)
+    return JsonResponse(signal)
 
 
 def neighborhood_view(request, state, name):
