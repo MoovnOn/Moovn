02dbab218c7670fb55d1d7d1f1d3d5072235d63e
Andy Baird <Andy.Baird@icloud.com>
2015-07-07 22:06:36 -0400

    added ajax call for housing data and var's that store objects for specifc types of housing data, change to bar chart

--- src/js/c3-charts.js
+++ src/js/c3-charts.js
@@ -1,16 +1,35 @@
 var c3 = require('c3');
-
+var $ = require('jquery');
 module.exports = function() {
-	c3.generate({
+	$.ajax({
+    url: '/api/homeprices/NC/Durham/',
+    method: 'GET'
+  })
+  .then(parseHousing);
+  
+  function parseHousing(allHousingData){
+    var housingResponse = allHousingData["Demographics:demographics"].response.pages.page;
+    var housingAfford= allHousingData["Demographics:demographics"].response.pages.page[0].tables.table.data.attribute;
+    var housingRealEstate= allHousingData["Demographics:demographics"].response.pages.page[1].tables.table;
+    var housingPeople= allHousingData["Demographics:demographics"].response.pages.page[2].tables.table;
+    
+    console.log(housingAfford);
+    console.log(housingRealEstate);
+    console.log(housingPeople);
+  }
+  
+  c3.generate({
     bindto: 'body .city-chart-container',
     data: {
       columns: [
-          ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 50, 20, 10, 40, 15, 25]
-      ]
+          ['Median 2 Bed Home', 30, 200, 100, 400, 150, 250],
+          ['data2', 400, 20, 10, 40, 15, 25]
+      ],
+      type: 'bar'
     },
+     
     size: {
-  		height: 480
+  		height: 400
 		}
    
 });
