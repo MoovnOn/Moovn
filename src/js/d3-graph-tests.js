d58a628dece2ad0cf53692a745ff58cf831b8377
John Joseph <jmj1444@gmail.com>
2015-07-09 15:02:26 -0400

    working on neighborhoods

--- src/js/d3-graph-tests.js
+++ src/js/d3-graph-tests.js
@@ -3,11 +3,34 @@ d3 = require('d3');
 
 module.exports = function() {
 
+// Gets liveshere descriptions
+	// $.ajax({
+	// 	method: 'GET',
+	// 	url: 'api/neighborhooddata/OR/Portland/'
+	// }).done(function(data) {
+	// 	var affordability = data['Demographics:demographics'].response.pages.page[0];
+	// 	var homes = data['Demographics:demographics'].response.pages.page[1];
+	// 	var people = data['Demographics:demographics'].response.pages.page[2];
+		
+	// 	var liveshereArray = people.segmentation.liveshere;
+
+	// 	liveshereArray.forEach(function(instance) {
+	// 		console.log(instance.name)
+	// 		console.log(instance.description)
+	// 	})
+
+	// })
+
+
 	$.ajax({
 		method: 'GET',
-		url: 'api/neighborhoods/Durham/NC/'
+		url: 'api/neighborhoods/NC/Raleigh/'
 	}).done(function(data) {
-		console.log(data);
-	})
+			var array = data.features;
+			array.forEach(function(item) {
+				console.log(item.properties.NAME);
+				console.log(item.properties.GEOID10);
+			})
 
+})
 };
