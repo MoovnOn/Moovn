7832d72412750961daa4bbf2bb09f1ece2704ca3
John Joseph <jmj1444@gmail.com>
2015-07-08 23:51:51 -0400

    working on graph

--- src/js/c3-charts.js
+++ src/js/c3-charts.js
@@ -1,7 +1,7 @@
 var c3 = require('c3');
 var d3 = require('d3');
 var $ = require('jquery');
-var cellChart = require('./cell-graphs');
+var cellArray = require('./cell-graphs');
 
 module.exports = function(state, city) {
 	console.log(state);
@@ -93,7 +93,57 @@ module.exports = function(state, city) {
 
       $('#chartType').change(function(){ 
          if ($('#cell').is(':selected')){
-            cellChart()
+            cellArray();
+
+            // var aTDwnld3 = Math.round(newArray[0].type3G.downloadSpeed);
+            // var vDwnld3 = Math.round(newArray[1].type3G.downloadSpeed);
+            // var sDwnld3 = Math.round(newArray[2].type3G.downloadSpeed);
+            // var tmDwnld3 = Math.round(newArray[3].type3G.downloadSpeed);
+            // var c1Dwnld3 = Math.round(newArray[4].type3G.downloadSpeed);
+
+            // var aTUpld3 = Math.round(newArray[0].type3G.uploadSpeed);
+            // var vUpld3 = Math.round(newArray[1].type3G.uploadSpeed);
+            // var sUpld3 = Math.round(newArray[2].type3G.uploadSpeed);
+            // var tmUpld3 = Math.round(newArray[3].type3G.uploadSpeed);
+            // var c1Upld3 = Math.round(newArray[4].type3G.uploadSpeed);
+
+            // var aTDwnld4 = Math.round(newArray[0].type4G.downloadSpeed);
+            // var vDwnld4 = Math.round(newArray[1].type4G.downloadSpeed);
+            // var sDwnld4 = Math.round(newArray[2].type4G.downloadSpeed);
+            // var tmDwnld4 = Math.round(newArray[3].type4G.downloadSpeed);
+
+            // var aTUpld4 = Math.round(newArray[0].type4G.uploadSpeed);
+            // var vUpld4 = Math.round(newArray[1].type4G.uploadSpeed);
+            // var sUpld4 = Math.round(newArray[2].type4G.uploadSpeed);
+            // var tmUpld4 = Math.round(newArray[3].type4G.uploadSpeed);
+
+
+            // chart.load({ 
+            //  data: {
+            //   columns: [
+            //     ['AT&T', aTDwnld3, aTUpld3, aTDwnld4, aTUpld4 ],
+            //     ['Verizon', vDwnld3, vUpld3, vDwnld4, vUpld4 ],
+            //     ['Sprint', sDwnld3, sUpld3, sDwnld4, sUpld4 ],
+            //     ['T-mobile', tmDwnld3, tmUpld3, tmDwnld4, tmUpld4 ],
+            //     ['Cellular-one', c1Dwnld3, c1Upld3]
+            //   ],
+            //   unload: ['Condo', '2-Bed-Home','3-Bed-Home','4-Bed-Home'],
+            //   type: 'bar',   
+            //   },
+            //   axis: {
+            //   x: {
+            //     type: 'category',
+            //     categories: ['3G-Download-Speed-Mbps', '3G-Upload-Speed-Mbps' , '4G-Download-Speed-Mbps', '4G-Upload-Speed-Mbps']
+            //     }
+            // }
+
+            // })
+
+
+
+
+
+    
 
            }
        })
--- src/js/cell-graphs.js
+++ src/js/cell-graphs.js
@@ -1,16 +1,18 @@
 var $ = require('jquery');
 var c3 = require('c3');
 
-module.exports = function() {
+module.exports = function () {
 
-	$.ajax({
+  
+
+  $.ajax({
   	method: 'GET',
   	url: 'api/celldata/NC/Durham/'
   }).done(function (data){	
 
   	var array = data.networkRank;
-  	var newArray = [];
-
+    var newArray = [];
+  	
   	array.forEach(function(prov) {
   		if (prov.networkName === "AT&T") {
   			newArray[0] = prov
@@ -28,54 +30,60 @@ module.exports = function() {
   			newArray[4] = prov
   		}
   	})
+  
+    console.log(newArray)
+  
+  });
 
-  	var aTDwnld3 = Math.round(data.networkRank[0].type3G.downloadSpeed);
-  	var vDwnld3 = Math.round(data.networkRank[1].type3G.downloadSpeed);
-		var sDwnld3 = Math.round(data.networkRank[2].type3G.downloadSpeed);
-		var tmDwnld3 = Math.round(data.networkRank[3].type3G.downloadSpeed);
-		var c1Dwnld3 = Math.round(data.networkRank[4].type3G.downloadSpeed);
-
-		var aTUpld3 = Math.round(data.networkRank[0].type3G.uploadSpeed);
-  	var vUpld3 = Math.round(data.networkRank[1].type3G.uploadSpeed);
-		var sUpld3 = Math.round(data.networkRank[2].type3G.uploadSpeed);
-		var tmUpld3 = Math.round(data.networkRank[3].type3G.uploadSpeed);
-		var c1Upld3 = Math.round(data.networkRank[4].type3G.uploadSpeed);
-
-		var aTDwnld4 = Math.round(data.networkRank[0].type4G.downloadSpeed);
-  	var vDwnld4 = Math.round(data.networkRank[1].type4G.downloadSpeed);
-		var sDwnld4 = Math.round(data.networkRank[2].type4G.downloadSpeed);
-		var tmDwnld4 = Math.round(data.networkRank[3].type4G.downloadSpeed);
-
-		var aTUpld4 = Math.round(data.networkRank[0].type4G.uploadSpeed);
-  	var vUpld4 = Math.round(data.networkRank[1].type4G.uploadSpeed);
-		var sUpld4 = Math.round(data.networkRank[2].type4G.uploadSpeed);
-		var tmUpld4 = Math.round(data.networkRank[3].type4G.uploadSpeed);
-
-
-
-
-  	var chart = c3.generate({
-    bindto: 'body .city-chart-container',
-    data: {
-        columns: [
-            ['AT&T', aTDwnld3, aTUpld3, aTDwnld4, aTUpld4 ],
-            ['Verizon', vDwnld3, vUpld3, vDwnld4, vUpld4 ],
-            ['Sprint', sDwnld3, sUpld3, sDwnld4, sUpld4 ],
-            ['T-mobile', tmDwnld3, tmUpld3, tmDwnld4, tmUpld4 ],
-            ['Cellular-one', c1Dwnld3, c1Upld3]
-        ],
-        type: 'bar',   
-  			},
-  	 		axis: {
-       		x: {
-            type: 'category',
-            categories: ['3G-Download-Speed-Mbps', '3G-Upload-Speed-Mbps' , '4G-Download-Speed-Mbps', '4G-Upload-Speed-Mbps']
-        	}
-    		},
-		});
- 	});	
 
 };
+
+    // var aTDwnld3 = Math.round(newArray[0].type3G.downloadSpeed);
+    // var vDwnld3 = Math.round(newArray[1].type3G.downloadSpeed);
+    // var sDwnld3 = Math.round(newArray[2].type3G.downloadSpeed);
+    // var tmDwnld3 = Math.round(newArray[3].type3G.downloadSpeed);
+    // var c1Dwnld3 = Math.round(newArray[4].type3G.downloadSpeed);
+
+    // var aTUpld3 = Math.round(newArray[0].type3G.uploadSpeed);
+    // var vUpld3 = Math.round(newArray[1].type3G.uploadSpeed);
+    // var sUpld3 = Math.round(newArray[2].type3G.uploadSpeed);
+    // var tmUpld3 = Math.round(newArray[3].type3G.uploadSpeed);
+    // var c1Upld3 = Math.round(newArray[4].type3G.uploadSpeed);
+
+    // var aTDwnld4 = Math.round(data.networkRank[0].type4G.downloadSpeed);
+    // var vDwnld4 = Math.round(data.networkRank[1].type4G.downloadSpeed);
+    // var sDwnld4 = Math.round(data.networkRank[2].type4G.downloadSpeed);
+    // var tmDwnld4 = Math.round(data.networkRank[3].type4G.downloadSpeed);
+
+    // var aTUpld4 = Math.round(data.networkRank[0].type4G.uploadSpeed);
+    // var vUpld4 = Math.round(data.networkRank[1].type4G.uploadSpeed);
+    // var sUpld4 = Math.round(data.networkRank[2].type4G.uploadSpeed);
+    // var tmUpld4 = Math.round(data.networkRank[3].type4G.uploadSpeed);
+
+
+
+  // 	var chart = c3.generate({
+  //   bindto: 'body .city-chart-container',
+  //   data: {
+  //       columns: [
+  //           ['AT&T', aTDwnld3, aTUpld3, aTDwnld4, aTUpld4 ],
+  //           ['Verizon', vDwnld3, vUpld3, vDwnld4, vUpld4 ],
+  //           ['Sprint', sDwnld3, sUpld3, sDwnld4, sUpld4 ],
+  //           ['T-mobile', tmDwnld3, tmUpld3, tmDwnld4, tmUpld4 ],
+  //           ['Cellular-one', c1Dwnld3, c1Upld3]
+  //       ],
+  //       type: 'bar',   
+  // 			},
+  // 	 		axis: {
+  //      		x: {
+  //           type: 'category',
+  //           categories: ['3G-Download-Speed-Mbps', '3G-Upload-Speed-Mbps' , '4G-Download-Speed-Mbps', '4G-Upload-Speed-Mbps']
+  //       	}
+  //   		},
+		// });
+ 	// });	
+
+
 
 
 // module.exports = function() {
