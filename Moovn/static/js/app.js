595e9e44151b6f47f63c68951ba93d1a454c9d47
Jeremy Gresham <greshjs@gmail.com>
2015-07-16 09:20:52 -0400

    make circle listeners work

--- Moovn/static/js/app.js
+++ Moovn/static/js/app.js
@@ -25267,19 +25267,26 @@ router.route('search/:cityName/housing', function (cityName){
   nTitle.append("span").style({"color": "darkgreen", "font-weight": "bold"})
     .text(city);
 
-  //$("#neighborhood-title").text("Select a Neighborhood");
 
-  var svg = d3.select("#d3-graphs");
-  var height = 400;
-  var width = 400;
-  svg.attr("width", width).attr("height", height);
+  var width = Math.max($("#d3-graphs").width(), 200),
+      aspect = 1;
 
-  // svg.append("text")
-  //   .attr("x", 200)
-  //   .attr("y", 20)
-  //   .attr("id", "map-title")
-  //   .attr("text-anchor", "middle")
-  //   .text("");
+  var svg = d3.select("#d3-graphs").append("svg")
+              .attr("preserveAspectRatio", "xMidYMid")
+              .attr("viewBox", "100 100 700 700")
+              .attr("width", width)
+              .attr("height", width * aspect)
+              .attr("class", "map");
+
+  // svg.attr("width", width).attr("height", height);
+
+  $(window).resize(function(){
+    var width = Math.max($(".quad-1").width(), 200);
+    svg.attr("width", width);
+    svg.attr("height", width * aspect);
+  });
+
+  //$(".map").on("resize", mouseOutZoom);
 
   var g = svg.append("g");
 
@@ -25292,20 +25299,19 @@ router.route('search/:cityName/housing', function (cityName){
   var boundaryjson = [];
   var id = 0;
 
-  // Promise.all([
-  //
-  // $.ajax({
-  //
-  //   method: 'GET',
-  //   url: '/api/boundary/' + 'US' + '/' + 'US' + '/'
-  //
-  // }).done(function (json){
-  //
-  //   neighMap(json, g, path, "black", "US");
-  //
-  // })
-  //
-  // ]).then(function(results){
+  var mouseOutZoom = function (d) {
+    $("#" + d.properties.GEOID10 + "T").attr("opacity", 0);
+    d3.selectAll("path")
+      .classed("active", false);
+    housingGraphGeneral(state, city);
+    return zoom(cityjson, boundaryjson, g, path, aspect * width, width);
+  };
+
+  var mouseZoom = function(d) {
+    $(".maptext").attr("opacity", 0);
+    $("#" + d.properties.GEOID10 + "T").attr("opacity", 1);
+    return mouseOverZoom(d, path, g, aspect * width, width, mouseOutZoom, state, city);
+  };
 
   Promise.all([
 
@@ -25316,7 +25322,7 @@ router.route('search/:cityName/housing', function (cityName){
 
       }).done(function (json){
 
-        cityjson = neighMap(json, g, path, "brown", "city", height, width);
+        cityjson = neighMap(json, g, path, "brown", "city", aspect * width, width);
 
       }),
 
@@ -25331,7 +25337,7 @@ router.route('search/:cityName/housing', function (cityName){
 
       }).done(function (json){
         if (json){
-          boundaryjson = neighMap(json, g, path, "grey", "neighborhood", height, width);
+          boundaryjson = neighMap(json, g, path, "grey", "neighborhood", aspect * width, width);
         } else {
           boundaryjson = false
         }
@@ -25341,37 +25347,19 @@ router.route('search/:cityName/housing', function (cityName){
     ]).then(function(results){
 
       if (boundaryjson){
-        zoom(cityjson, boundaryjson, g, path, height, width);
-
-        var mouseOutZoom = function (d) {
-          $("#" + d.properties.GEOID10 + "T").attr("opacity", 0);
-
-          d3.selectAll("path")
-            .classed("active", false)
-          //c3.generate(housingdata);
-          housingGraphGeneral(state, city);
-          return zoom(cityjson, boundaryjson, g, path, height, width);
-        };
-
-        var mouseZoom = function(d) {
-          $(".maptext").attr("opacity", 0);
-          $("#" + d.properties.GEOID10 + "T").attr("opacity", 1);
-          return mouseOverZoom(d, path, g, height, width, mouseOutZoom, state, city);
-        };
-
+        zoom(cityjson, boundaryjson, g, path, aspect * width, width);
         d3.selectAll(".feature-neighborhoodTP").on("click", mouseZoom);
 
       } else {
 
-        zoom(cityjson, cityjson, g, path, height, width);
+        zoom(cityjson, cityjson, g, path, aspect * width, width);
 
       }
 
     });
 
   });
-  //
-  // })
+
 
   activeSelection();
 
@@ -26236,16 +26224,22 @@ $ = require('jquery');
 
 module.exports = function(svg, state, city) {
 
-	$.ajax({
-		url: "/api/industrysize/" + state + "/" + city + "/"
-	}).done(function(d){
-		//console.log(d);
-		bubbleChart(d);
-	});
+	var showText = function (d) {
+		console.log(this)
+		d3.select("#text" + this.id).attr("opacity", 1);
+	};
+
+	var hideText = function () {
+
+
+	};
+
+	var toggleText = function () {
+
+
+	};
 
 	var bubbleChart = function (data) {
-		//console.log("data");
-		//console.log(data);
 
 		var data_list = {"children":[]};
 
@@ -26261,9 +26255,7 @@ module.exports = function(svg, state, city) {
 			cb(key, data);
 		}
 
-		console.log(svg)
-		var diameter = Math.min(svg.attr("height"),svg.attr("width")) / 2;
-		console.log(diameter)
+		var diameter = 200;
 		var color = d3.scale.category20b();
 
 		var bubble = d3.layout.pack()
@@ -26271,8 +26263,22 @@ module.exports = function(svg, state, city) {
 					.size([diameter, diameter])
 					.padding(.5);
 
-		console.log(bubble.nodes(data_list))
+		//console.log(bubble.nodes(data_list))
 		g = svg.append("g")
+
+		var counter = function (){
+			var k = 0;
+			var m = function () {
+				k = k + 1;
+				return k;
+			};
+
+			return m;
+		};
+
+		var count = counter();
+		var count2 = counter();
+
 		var node = g.selectAll(".node")
 									.data(bubble.nodes(data_list))
 								.enter().append("g")
@@ -26282,19 +26288,33 @@ module.exports = function(svg, state, city) {
 
 		node.append("circle")
 				.attr("r", function(d){ return d.r;})
+				.attr("class", "circle")
+				.attr("id", function(d){
+					return count();})
 				.attr("fill", function(d){ return color(d.name);});
-
 
 		var text = node.append("g")
 									 .append("text")
 		  					 	 .style("text-anchor", "middle")
+									 .attr("opacity", 0)
+									 .attr("id", function(d){return "text" + count2();})
 		  					   .text(function(d){ return d.name;});
 
-		text.attr("transform", function(d){ return "scale(" + 1/2  + ")";})
+		text.attr("transform", function(d){ return "scale(" + 1 / 2 + ")";})
 
 		g.attr("transform", "scale(" + 2 + ")")
 
 	};
+
+	$.ajax({
+		url: "/api/industrysize/" + state + "/" + city + "/"
+	}).done(function(d){
+		bubbleChart(d);
+	}).done(function() {
+		var circles = d3.selectAll(".circle").on("click", showText);
+		// circles.on("mouseenter", showText);
+		// circles.on("mouseout", hideText);
+	});
 
 };
 
@@ -26808,14 +26828,14 @@ module.exports = function (json, g, path, color, type, height, width) {
   if (type !== "neighborhood"){
 
     g.selectAll("path")
-        .data(data.features, function(d){return d.properties.GEOID10;})
+        .data(data.features, function (d) { return d.properties.GEOID10;})
       .enter().append("path")
         .attr("d", path)
         .attr("class", "feature-" + type)
         .style("fill", fill)
         .style("fill-opacity", opacity)
         .style("stroke", stroke)
-        .attr("id", function(d){return d.properties.GEOID10;});
+        .attr("id", function (d) { return d.properties.GEOID10;});
 
   }else{
 
@@ -26824,7 +26844,7 @@ module.exports = function (json, g, path, color, type, height, width) {
     neighG.attr("class", "neighborhoods");
 
     neighG.selectAll("path")
-        .data(data.features, function(d){return d.properties.GEOID10;})
+        .data(data.features, function (d) { return d.properties.GEOID10;})
       .enter().append("path")
         .attr("d", path)
         .attr("class", "feature-" + type)
@@ -26832,7 +26852,7 @@ module.exports = function (json, g, path, color, type, height, width) {
         .style("fill-opacity", opacity)
         .style("stroke", stroke)
         .style("stroke-opacity", 0.1)
-        .attr("id", function(d){return d.properties.GEOID10;});
+        .attr("id", function (d) { return d.properties.GEOID10;});
 
     var text = neighG.append("g")
 
@@ -26843,14 +26863,14 @@ module.exports = function (json, g, path, color, type, height, width) {
     text.selectAll("text")
       .data(data.features)
     .enter().append("text")
-      .attr("transform", function(d){ return "translate(" + path.centroid(d) +
+      .attr("transform", function (d) { return "translate(" + path.centroid(d) +
             ")scale(" + 3 / (scale(path.bounds(d))) +")";})
-      .attr("id", function(d){return d.properties.GEOID10 + "T";})
+      .attr("id", function (d) { return d.properties.GEOID10 + "T";})
       .attr("opacity", 0)
       .attr("class", "maptext")
       .style("user-select", "none")
       .attr("text-anchor", "middle")
-      .text(function(d){ return d.properties.NAME;});
+      .text(function (d) { return d.properties.NAME;});
 
     var neighT = neighG.append("g")
     neighT.selectAll("path")
@@ -26861,7 +26881,7 @@ module.exports = function (json, g, path, color, type, height, width) {
         .style("fill", "white")
         .style("fill-opacity", 0)
         .style("stroke", "none")
-        .attr("id", function(d){return d.properties.GEOID10 + "TP";});
+        .attr("id", function (d) { return d.properties.GEOID10 + "TP";});
 
 
   }
@@ -27112,15 +27132,12 @@ var clicked = function (){
       var translate = [width / 2 - scale * x, height / 2 - scale * y];
 
     g.transition()
-     .duration(250)
-     //.style("stroke-width", 1.5/ scale + "px")
+     .duration(750)
      .call(zoomMap.translate(translate).scale(scale).event);
-
-    //zoomed(translate, scale);
 
 }
 
-  function zoomed(translate, scale){
+  function zoomed(d){
 
     g.style("stroke-width", 1.5 / d3.event.scale + "px");
     g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
